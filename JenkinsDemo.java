d06d6fa22bbdc8e9810bc44bbb793a9528214f38
nasirabanu <nasirabanu.j@gmail.com>
2018-07-22 12:29:06 +0530

    sample file

---
+++ JenkinsDemo.java
@@ -0,0 +1,23 @@
+package com.parallelexecution;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JenkinsDemo {
+	WebDriver driver;
+	@Test
+public void demo(){
+	
+		System.out.println("first demo");
+		/*System.setProperty("webdriver.chrome.driver","C:\\Users\\Krishna\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		//driver.manage().window().maximize();
+		driver.get("http://www.facebook.com");
+		System.out.println(driver.getTitle());
+		//driver.get("https://www.dovemed.com/admin/");
+		driver.quit();*/
+		
+			}
+
+}
