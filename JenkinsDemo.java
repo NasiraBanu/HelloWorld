26d092b770e30cad12dbdc079cf362bf84501446
nasirabanu <nasirabanu.j@gmail.com>
2018-07-28 12:06:18 +0530

    changed

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -6,10 +6,13 @@ import org.testng.annotations.Test;
 
 public class JenkinsDemo {
 	WebDriver driver;
+WebDriver driver1;
 	@Test
 public void demo(){
 	
 		System.out.println("first demo");
+System.out.println("first demo1");
+
 		/*System.setProperty("webdriver.chrome.driver","C:\\Users\\Krishna\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		//driver.manage().window().maximize();
